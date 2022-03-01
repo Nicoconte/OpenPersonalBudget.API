3df11825b25ea2a891569797cf0da750e421a41a
NicoConte <nicoconte1999@gmail.com>
2022-02-28 22:54:52 -0300

    New Validators for operations

---
+++ Validators/UpdateOperationValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using OpenPersonalBudget.API.Contracts.Requests;
+
+namespace OpenPersonalBudget.API.Validators
+{
+    public class UpdateOperationValidator : AbstractValidator<UpdateOperationRequest>
+    {
+
+        public UpdateOperationValidator()
+        {
+            RuleFor(o => o.Amount)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Amount cannot be empty")
+                .LessThan(0)
+                .WithMessage("Amount cannot be negative")
+                .Must(f => f.GetType() == typeof(float))
+                .WithMessage("Amount should be float");
+
+            RuleFor(o => o.Description)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Description cannot be empty");
+
+
+            RuleFor(o => o.Category)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Category cannot be empty");
+
+            RuleFor(o => o.OperationType)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("OperationType cannot be empty");
+
+            RuleFor(o => o.PaymentType)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("PaymentType cannot be empty");
+        }
+
+    }
+}
